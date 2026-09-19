0c9004c479af8556aa51da7a7b3ad210031ad779
Seth Curry <seth@scurry.io>
2026-09-18 20:17:26 -0400

    ACPPluginRouter mostly written

--- pkg/abyss/message_type.go
+++ pkg/abyss/message_type.go
@@ -1,11 +1,18 @@
 package abyss
 
-type MessageType int32
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/coder/acp-go-sdk"
+)
+
+type MessageTypeID int32
 
 const (
-	MessageTypeNotExist MessageType = iota
+	MessageTypeNotExist MessageTypeID = iota
 
-	// Client request/response types (websocket_client.go)
 	RequestPermissionRequestType
 	RequestPermissionResponseType
 	WriteTextFileRequestType
@@ -24,7 +31,6 @@ const (
 	KillTerminalResponseType
 	SessionNotificationType
 
-	// Agent request/response types (websocket_agent.go)
 	SetSessionModeRequestType
 	SetSessionModeResponseType
 	UnstableForkSessionRequestType
@@ -72,3 +78,221 @@ const (
 	PromptResponseType
 	CancelNotificationType
 )
+
+// MessageDirection represents the direction of a message,
+// either "to_acp_client" or "to_agent".
+type MessageDirection string
+
+const (
+	// ToACPClient is the direction of a message sent from the agent to the ACP client.
+	ToACPClient MessageDirection = "to_acp_client"
+
+	// ToAgent is the direction of a message sent from the ACP client to the agent.
+	ToAgent MessageDirection = "to_agent"
+)
+
+func newMessageTypeT[T any](msgTypeID MessageTypeID, direction MessageDirection) *MessageTypeT[T] {
+	return &MessageTypeT[T]{
+		typeID:    msgTypeID,
+		direction: direction,
+	}
+}
+
+// MessageTypeT is a generic struct that holds a message type's ID and direction.
+// It is primarily used to unmarshal ACP messages by their MessageTypeID
+type MessageTypeT[T any] struct {
+	typeID    MessageTypeID
+	direction MessageDirection
+}
+
+// Unmarshal unmarshals the content into the message's concrete type.
+func (m *MessageTypeT[T]) Unmarshal(content []byte) (T, error) {
+	var resp T
+	err := json.Unmarshal(content, &resp)
+	if err != nil {
+		return resp, err
+	}
+	return resp, nil
+}
+
+// TypeID returns the identifier for this message type.
+func (m *MessageTypeT[T]) TypeID() MessageTypeID { return m.typeID }
+
+// Type returns the concrete Go type this message unmarshals into.
+func (m *MessageTypeT[T]) Type() reflect.Type { return reflect.TypeOf((*T)(nil)).Elem() }
+
+// UnmarshalAny unmarshals content into the message's concrete type, returned as any.
+func (m *MessageTypeT[T]) UnmarshalAny(content []byte) (any, error) {
+	return m.Unmarshal(content)
+}
+
+// Direction returns the direction of this message type.
+func (m *MessageTypeT[T]) Direction() MessageDirection { return m.direction }
+
+// TypedMessage is the common interface for generic Message[T] values so they can
+// be stored together in a single heterogeneous registry.
+type TypedMessage interface {
+	TypeID() MessageTypeID
+	Type() reflect.Type
+	UnmarshalAny(content []byte) (any, error)
+	Direction() MessageDirection
+}
+
+// GetMessageTypeByID returns the TypedMessage for the given message type ID,
+// or an error if not found.
+func GetMessageTypeByID(msgTypeID int32) (TypedMessage, error) {
+	for _, v := range AllMessageTypes {
+		if int32(v.TypeID()) == msgTypeID {
+			return v, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no ACP message type with ID %d", msgTypeID)
+}
+
+// GetMessageTypeByType returns the TypedMessage for the given message,
+// or an error if not found (or if the message type is not an ACP message).
+func GetMessageTypeByType(msg any) (TypedMessage, error) {
+	msgType := reflect.TypeOf(msg)
+	for _, v := range AllMessageTypes {
+		if msgType == v.Type() {
+			return v, nil
+		}
+	}
+
+	return nil, fmt.Errorf("unknown router message type %T", msg)
+}
+
+var (
+	RequestPermissionRequestMsg        = newMessageTypeT[acp.RequestPermissionRequest](RequestPermissionRequestType, ToACPClient)
+	RequestPermissionResponseMsg       = newMessageTypeT[acp.RequestPermissionResponse](RequestPermissionResponseType, ToAgent)
+	WriteTextFileRequestMsg            = newMessageTypeT[acp.WriteTextFileRequest](WriteTextFileRequestType, ToACPClient)
+	WriteTextFileResponseMsg           = newMessageTypeT[acp.WriteTextFileResponse](WriteTextFileResponseType, ToAgent)
+	ReadTextFileRequestMsg             = newMessageTypeT[acp.ReadTextFileRequest](ReadTextFileRequestType, ToACPClient)
+	ReadTextFileResponseMsg            = newMessageTypeT[acp.ReadTextFileResponse](ReadTextFileResponseType, ToAgent)
+	CreateTerminalRequestMsg           = newMessageTypeT[acp.CreateTerminalRequest](CreateTerminalRequestType, ToACPClient)
+	CreateTerminalResponseMsg          = newMessageTypeT[acp.CreateTerminalResponse](CreateTerminalResponseType, ToAgent)
+	TerminalOutputRequestMsg           = newMessageTypeT[acp.TerminalOutputRequest](TerminalOutputRequestType, ToACPClient)
+	TerminalOutputResponseMsg          = newMessageTypeT[acp.TerminalOutputResponse](TerminalOutputResponseType, ToAgent)
+	ReleaseTerminalRequestMsg          = newMessageTypeT[acp.ReleaseTerminalRequest](ReleaseTerminalRequestType, ToACPClient)
+	ReleaseTerminalResponseMsg         = newMessageTypeT[acp.ReleaseTerminalResponse](ReleaseTerminalResponseType, ToAgent)
+	WaitForTerminalExitRequestMsg      = newMessageTypeT[acp.WaitForTerminalExitRequest](WaitForTerminalExitRequestType, ToACPClient)
+	WaitForTerminalExitResponseMsg     = newMessageTypeT[acp.WaitForTerminalExitResponse](WaitForTerminalExitResponseType, ToAgent)
+	KillTerminalRequestMsg             = newMessageTypeT[acp.KillTerminalRequest](KillTerminalRequestType, ToACPClient)
+	KillTerminalResponseMsg            = newMessageTypeT[acp.KillTerminalResponse](KillTerminalResponseType, ToAgent)
+	SessionNotificationMsg             = newMessageTypeT[acp.SessionNotification](SessionNotificationType, ToACPClient)
+	SetSessionModeRequestMsg           = newMessageTypeT[acp.SetSessionModeRequest](SetSessionModeRequestType, ToAgent)
+	SetSessionModeResponseMsg          = newMessageTypeT[acp.SetSessionModeResponse](SetSessionModeResponseType, ToACPClient)
+	UnstableForkSessionRequestMsg      = newMessageTypeT[acp.UnstableForkSessionRequest](UnstableForkSessionRequestType, ToAgent)
+	UnstableForkSessionResponseMsg     = newMessageTypeT[acp.UnstableForkSessionResponse](UnstableForkSessionResponseType, ToACPClient)
+	ListSessionsRequestMsg             = newMessageTypeT[acp.ListSessionsRequest](ListSessionsRequestType, ToAgent)
+	ListSessionsResponseMsg            = newMessageTypeT[acp.ListSessionsResponse](ListSessionsResponseType, ToACPClient)
+	ResumeSessionRequestMsg            = newMessageTypeT[acp.ResumeSessionRequest](ResumeSessionRequestType, ToAgent)
+	ResumeSessionResponseMsg           = newMessageTypeT[acp.ResumeSessionResponse](ResumeSessionResponseType, ToACPClient)
+	SetSessionConfigOptionRequestMsg   = newMessageTypeT[acp.SetSessionConfigOptionRequest](SetSessionConfigOptionRequestType, ToAgent)
+	SetSessionConfigOptionResponseMsg  = newMessageTypeT[acp.SetSessionConfigOptionResponse](SetSessionConfigOptionResponseType, ToACPClient)
+	LogoutRequestMsg                   = newMessageTypeT[acp.LogoutRequest](LogoutRequestType, ToAgent)
+	LogoutResponseMsg                  = newMessageTypeT[acp.LogoutResponse](LogoutResponseType, ToACPClient)
+	UnstableCloseNesRequestMsg         = newMessageTypeT[acp.UnstableCloseNesRequest](UnstableCloseNesRequestType, ToAgent)
+	UnstableCloseNesResponseMsg        = newMessageTypeT[acp.UnstableCloseNesResponse](UnstableCloseNesResponseType, ToACPClient)
+	UnstableStartNesRequestMsg         = newMessageTypeT[acp.UnstableStartNesRequest](UnstableStartNesRequestType, ToAgent)
+	UnstableStartNesResponseMsg        = newMessageTypeT[acp.UnstableStartNesResponse](UnstableStartNesResponseType, ToACPClient)
+	UnstableSuggestNesRequestMsg       = newMessageTypeT[acp.UnstableSuggestNesRequest](UnstableSuggestNesRequestType, ToAgent)
+	UnstableSuggestNesResponseMsg      = newMessageTypeT[acp.UnstableSuggestNesResponse](UnstableSuggestNesResponseType, ToACPClient)
+	UnstableAcceptNesNotificationMsg   = newMessageTypeT[acp.UnstableAcceptNesNotification](UnstableAcceptNesNotificationType, ToAgent)
+	UnstableRejectNesNotificationMsg   = newMessageTypeT[acp.UnstableRejectNesNotification](UnstableRejectNesNotificationType, ToAgent)
+	UnstableDidChangeDocumentNotifMsg  = newMessageTypeT[acp.UnstableDidChangeDocumentNotification](UnstableDidChangeDocumentNotificationType, ToAgent)
+	UnstableDidCloseDocumentNotifMsg   = newMessageTypeT[acp.UnstableDidCloseDocumentNotification](UnstableDidCloseDocumentNotificationType, ToAgent)
+	UnstableDidFocusDocumentNotifMsg   = newMessageTypeT[acp.UnstableDidFocusDocumentNotification](UnstableDidFocusDocumentNotificationType, ToAgent)
+	UnstableDidOpenDocumentNotifMsg    = newMessageTypeT[acp.UnstableDidOpenDocumentNotification](UnstableDidOpenDocumentNotificationType, ToAgent)
+	UnstableDidSaveDocumentNotifMsg    = newMessageTypeT[acp.UnstableDidSaveDocumentNotification](UnstableDidSaveDocumentNotificationType, ToAgent)
+	UnstableDisableProviderRequestMsg  = newMessageTypeT[acp.UnstableDisableProviderRequest](UnstableDisableProviderRequestType, ToAgent)
+	UnstableDisableProviderResponseMsg = newMessageTypeT[acp.UnstableDisableProviderResponse](UnstableDisableProviderResponseType, ToACPClient)
+	UnstableListProvidersRequestMsg    = newMessageTypeT[acp.UnstableListProvidersRequest](UnstableListProvidersRequestType, ToAgent)
+	UnstableListProvidersResponseMsg   = newMessageTypeT[acp.UnstableListProvidersResponse](UnstableListProvidersResponseType, ToACPClient)
+	UnstableSetProviderRequestMsg      = newMessageTypeT[acp.UnstableSetProviderRequest](UnstableSetProviderRequestType, ToAgent)
+	UnstableSetProviderResponseMsg     = newMessageTypeT[acp.UnstableSetProviderResponse](UnstableSetProviderResponseType, ToACPClient)
+	UnstableDeleteSessionRequestMsg    = newMessageTypeT[acp.UnstableDeleteSessionRequest](UnstableDeleteSessionRequestType, ToAgent)
+	UnstableDeleteSessionResponseMsg   = newMessageTypeT[acp.UnstableDeleteSessionResponse](UnstableDeleteSessionResponseType, ToACPClient)
+	CloseSessionRequestMsg             = newMessageTypeT[acp.CloseSessionRequest](CloseSessionRequestType, ToAgent)
+	CloseSessionResponseMsg            = newMessageTypeT[acp.CloseSessionResponse](CloseSessionResponseType, ToACPClient)
+	InitializeRequestMsg               = newMessageTypeT[acp.InitializeRequest](InitializeRequestType, ToAgent)
+	InitializeResponseMsg              = newMessageTypeT[acp.InitializeResponse](InitializeResponseType, ToACPClient)
+	NewSessionRequestMsg               = newMessageTypeT[acp.NewSessionRequest](NewSessionRequestType, ToAgent)
+	NewSessionResponseMsg              = newMessageTypeT[acp.NewSessionResponse](NewSessionResponseType, ToACPClient)
+	AuthenticateRequestMsg             = newMessageTypeT[acp.AuthenticateRequest](AuthenticateRequestType, ToAgent)
+	AuthenticateResponseMsg            = newMessageTypeT[acp.AuthenticateResponse](AuthenticateResponseType, ToACPClient)
+	LoadSessionRequestMsg              = newMessageTypeT[acp.LoadSessionRequest](LoadSessionRequestType, ToAgent)
+	LoadSessionResponseMsg             = newMessageTypeT[acp.LoadSessionResponse](LoadSessionResponseType, ToACPClient)
+	PromptRequestMsg                   = newMessageTypeT[acp.PromptRequest](PromptRequestType, ToAgent)
+	PromptResponseMsg                  = newMessageTypeT[acp.PromptResponse](PromptResponseType, ToACPClient)
+	CancelNotificationMsg              = newMessageTypeT[acp.CancelNotification](CancelNotificationType, ToAgent)
+)
+
+// AllMessageTypes is a list of all ACP message types.
+// Used for looking up message types by ID or type.
+var AllMessageTypes = []TypedMessage{
+	RequestPermissionRequestMsg,
+	RequestPermissionResponseMsg,
+	WriteTextFileRequestMsg,
+	WriteTextFileResponseMsg,
+	ReadTextFileRequestMsg,
+	ReadTextFileResponseMsg,
+	CreateTerminalRequestMsg,
+	CreateTerminalResponseMsg,
+	TerminalOutputRequestMsg,
+	TerminalOutputResponseMsg,
+	ReleaseTerminalRequestMsg,
+	ReleaseTerminalResponseMsg,
+	WaitForTerminalExitRequestMsg,
+	WaitForTerminalExitResponseMsg,
+	KillTerminalRequestMsg,
+	KillTerminalResponseMsg,
+	SessionNotificationMsg,
+	SetSessionModeRequestMsg,
+	SetSessionModeResponseMsg,
+	UnstableForkSessionRequestMsg,
+	UnstableForkSessionResponseMsg,
+	ListSessionsRequestMsg,
+	ListSessionsResponseMsg,
+	ResumeSessionRequestMsg,
+	ResumeSessionResponseMsg,
+	SetSessionConfigOptionRequestMsg,
+	SetSessionConfigOptionResponseMsg,
+	LogoutRequestMsg,
+	LogoutResponseMsg,
+	UnstableCloseNesRequestMsg,
+	UnstableCloseNesResponseMsg,
+	UnstableStartNesRequestMsg,
+	UnstableStartNesResponseMsg,
+	UnstableSuggestNesRequestMsg,
+	UnstableSuggestNesResponseMsg,
+	UnstableAcceptNesNotificationMsg,
+	UnstableRejectNesNotificationMsg,
+	UnstableDidChangeDocumentNotifMsg,
+	UnstableDidCloseDocumentNotifMsg,
+	UnstableDidFocusDocumentNotifMsg,
+	UnstableDidOpenDocumentNotifMsg,
+	UnstableDidSaveDocumentNotifMsg,
+	UnstableDisableProviderRequestMsg,
+	UnstableDisableProviderResponseMsg,
+	UnstableListProvidersRequestMsg,
+	UnstableListProvidersResponseMsg,
+	UnstableSetProviderRequestMsg,
+	UnstableSetProviderResponseMsg,
+	UnstableDeleteSessionRequestMsg,
+	UnstableDeleteSessionResponseMsg,
+	CloseSessionRequestMsg,
+	CloseSessionResponseMsg,
+	InitializeRequestMsg,
+	InitializeResponseMsg,
+	NewSessionRequestMsg,
+	NewSessionResponseMsg,
+	AuthenticateRequestMsg,
+	AuthenticateResponseMsg,
+	LoadSessionRequestMsg,
+	LoadSessionResponseMsg,
+	PromptRequestMsg,
+	PromptResponseMsg,
+	CancelNotificationMsg,
+}
---
+++ pkg/abyss/plugin.go
@@ -0,0 +1,165 @@
+package abyss
+
+import (
+	"encoding/json"
+
+	"github.com/SethCurry/abyss/pkg/protobyss"
+	"github.com/coder/acp-go-sdk"
+)
+
+// ACPPlugin is a typed view of an ACP plugin: each ACP message type is
+// dispatched to a dedicated handler so plugin authors implement typed methods
+// rather than a single raw HandleMessage switch.
+type ACPPlugin interface {
+	// Client capability requests (agent -> client).
+	OnRequestPermissionRequest(acp.RequestPermissionRequest) ([]*protobyss.ACPContainer, error)
+	OnWriteTextFileRequest(acp.WriteTextFileRequest) ([]*protobyss.ACPContainer, error)
+	OnReadTextFileRequest(acp.ReadTextFileRequest) ([]*protobyss.ACPContainer, error)
+	OnCreateTerminalRequest(acp.CreateTerminalRequest) ([]*protobyss.ACPContainer, error)
+	OnTerminalOutputRequest(acp.TerminalOutputRequest) ([]*protobyss.ACPContainer, error)
+	OnReleaseTerminalRequest(acp.ReleaseTerminalRequest) ([]*protobyss.ACPContainer, error)
+	OnWaitForTerminalExitRequest(acp.WaitForTerminalExitRequest) ([]*protobyss.ACPContainer, error)
+	OnKillTerminalRequest(acp.KillTerminalRequest) ([]*protobyss.ACPContainer, error)
+	OnSessionNotification(acp.SessionNotification) error
+
+	// Agent requests (client -> agent).
+	OnAuthenticateRequest(acp.AuthenticateRequest) ([]*protobyss.ACPContainer, error)
+	OnInitializeRequest(acp.InitializeRequest) ([]*protobyss.ACPContainer, error)
+	OnLogoutRequest(acp.LogoutRequest) ([]*protobyss.ACPContainer, error)
+	OnCancelNotification(acp.CancelNotification) error
+	OnCloseSessionRequest(acp.CloseSessionRequest) ([]*protobyss.ACPContainer, error)
+	OnListSessionsRequest(acp.ListSessionsRequest) ([]*protobyss.ACPContainer, error)
+	OnNewSessionRequest(acp.NewSessionRequest) ([]*protobyss.ACPContainer, error)
+	OnPromptRequest(acp.PromptRequest) ([]*protobyss.ACPContainer, error)
+	OnResumeSessionRequest(acp.ResumeSessionRequest) ([]*protobyss.ACPContainer, error)
+	OnSetSessionConfigOptionRequest(acp.SetSessionConfigOptionRequest) ([]*protobyss.ACPContainer, error)
+	OnSetSessionModeRequest(acp.SetSessionModeRequest) ([]*protobyss.ACPContainer, error)
+	OnLoadSessionRequest(acp.LoadSessionRequest) ([]*protobyss.ACPContainer, error)
+
+	// Experimental agent requests (client -> agent).
+	OnUnstableDidChangeDocumentNotification(acp.UnstableDidChangeDocumentNotification) error
+	OnUnstableDidCloseDocumentNotification(acp.UnstableDidCloseDocumentNotification) error
+	OnUnstableDidFocusDocumentNotification(acp.UnstableDidFocusDocumentNotification) error
+	OnUnstableDidOpenDocumentNotification(acp.UnstableDidOpenDocumentNotification) error
+	OnUnstableDidSaveDocumentNotification(acp.UnstableDidSaveDocumentNotification) error
+	OnUnstableAcceptNesNotification(acp.UnstableAcceptNesNotification) error
+	OnUnstableCloseNesRequest(acp.UnstableCloseNesRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableRejectNesNotification(acp.UnstableRejectNesNotification) error
+	OnUnstableStartNesRequest(acp.UnstableStartNesRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableSuggestNesRequest(acp.UnstableSuggestNesRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableDisableProviderRequest(acp.UnstableDisableProviderRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableListProvidersRequest(acp.UnstableListProvidersRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableSetProviderRequest(acp.UnstableSetProviderRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableDeleteSessionRequest(acp.UnstableDeleteSessionRequest) ([]*protobyss.ACPContainer, error)
+	OnUnstableForkSessionRequest(acp.UnstableForkSessionRequest) ([]*protobyss.ACPContainer, error)
+}
+
+type ACPPluginRouter struct {
+	underlying ACPPlugin
+}
+
+func (r *ACPPluginRouter) Handle(msg *protobyss.ACPContainer) ([]*protobyss.ACPContainer, error) {
+	switch MessageTypeID(msg.TypeId) {
+	// Client capability requests (agent -> client).
+	case RequestPermissionRequestType:
+		return handleRequest(msg, r.underlying.OnRequestPermissionRequest)
+	case WriteTextFileRequestType:
+		return handleRequest(msg, r.underlying.OnWriteTextFileRequest)
+	case ReadTextFileRequestType:
+		return handleRequest(msg, r.underlying.OnReadTextFileRequest)
+	case CreateTerminalRequestType:
+		return handleRequest(msg, r.underlying.OnCreateTerminalRequest)
+	case TerminalOutputRequestType:
+		return handleRequest(msg, r.underlying.OnTerminalOutputRequest)
+	case ReleaseTerminalRequestType:
+		return handleRequest(msg, r.underlying.OnReleaseTerminalRequest)
+	case WaitForTerminalExitRequestType:
+		return handleRequest(msg, r.underlying.OnWaitForTerminalExitRequest)
+	case KillTerminalRequestType:
+		return handleRequest(msg, r.underlying.OnKillTerminalRequest)
+	case SessionNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnSessionNotification)
+
+	// Agent requests (client -> agent).
+	case AuthenticateRequestType:
+		return handleRequest(msg, r.underlying.OnAuthenticateRequest)
+	case InitializeRequestType:
+		return handleRequest(msg, r.underlying.OnInitializeRequest)
+	case LogoutRequestType:
+		return handleRequest(msg, r.underlying.OnLogoutRequest)
+	case CancelNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnCancelNotification)
+	case CloseSessionRequestType:
+		return handleRequest(msg, r.underlying.OnCloseSessionRequest)
+	case ListSessionsRequestType:
+		return handleRequest(msg, r.underlying.OnListSessionsRequest)
+	case NewSessionRequestType:
+		return handleRequest(msg, r.underlying.OnNewSessionRequest)
+	case PromptRequestType:
+		return handleRequest(msg, r.underlying.OnPromptRequest)
+	case ResumeSessionRequestType:
+		return handleRequest(msg, r.underlying.OnResumeSessionRequest)
+	case SetSessionConfigOptionRequestType:
+		return handleRequest(msg, r.underlying.OnSetSessionConfigOptionRequest)
+	case SetSessionModeRequestType:
+		return handleRequest(msg, r.underlying.OnSetSessionModeRequest)
+	case LoadSessionRequestType:
+		return handleRequest(msg, r.underlying.OnLoadSessionRequest)
+
+	// Experimental agent requests (client -> agent).
+	case UnstableDidChangeDocumentNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableDidChangeDocumentNotification)
+	case UnstableDidCloseDocumentNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableDidCloseDocumentNotification)
+	case UnstableDidFocusDocumentNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableDidFocusDocumentNotification)
+	case UnstableDidOpenDocumentNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableDidOpenDocumentNotification)
+	case UnstableDidSaveDocumentNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableDidSaveDocumentNotification)
+	case UnstableAcceptNesNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableAcceptNesNotification)
+	case UnstableCloseNesRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableCloseNesRequest)
+	case UnstableRejectNesNotificationType:
+		return nil, handleNotification(msg, r.underlying.OnUnstableRejectNesNotification)
+	case UnstableStartNesRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableStartNesRequest)
+	case UnstableSuggestNesRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableSuggestNesRequest)
+	case UnstableDisableProviderRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableDisableProviderRequest)
+	case UnstableListProvidersRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableListProvidersRequest)
+	case UnstableSetProviderRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableSetProviderRequest)
+	case UnstableDeleteSessionRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableDeleteSessionRequest)
+	case UnstableForkSessionRequestType:
+		return handleRequest(msg, r.underlying.OnUnstableForkSessionRequest)
+
+	default:
+		// Pass unhandled messages through unchanged.
+		return []*protobyss.ACPContainer{msg}, nil
+	}
+}
+
+// handleRequest unmarshals msg into T, invokes fn, and returns its response.
+func handleRequest[T any](msg *protobyss.ACPContainer, fn func(T) ([]*protobyss.ACPContainer, error)) ([]*protobyss.ACPContainer, error) {
+	var params T
+	if err := json.Unmarshal(msg.Content, &params); err != nil {
+		return nil, err
+	}
+
+	return fn(params)
+}
+
+// handleNotification unmarshals msg into T and invokes fn for its side effects.
+func handleNotification[T any](msg *protobyss.ACPContainer, fn func(T) error) error {
+	var params T
+	if err := json.Unmarshal(msg.Content, &params); err != nil {
+		return err
+	}
+
+	return fn(params)
+}
